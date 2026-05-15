b7943aed38081dc86d07f91f95ffdc02daf8d8eb
evitsapara <evi.tsapi@gmail.com>
2026-05-15 18:11:33 +0300

    feat: add TemperatureInput and TemperatureResult components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Layout from "./components/Layout.tsx";
+import Converter from "./components/converter_components/Converter.tsx";
 
 function App() {
 
   return (
     <>
       <Layout>
+          <Converter />
       </Layout>
     </>
   )
