a091dd727d05ad169f149f52dfc5d8f46a04bc48
evitsapara <evi.tsapi@gmail.com>
2026-05-15 18:11:04 +0300

    feat(ui): implement ui on layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
                         <img src="https://www.pngkey.com/png/detail/297-2976588_temperature-conversion-convert-temperature-icon.png" alt="Temperature Conversion" className="my-4 h-16"/>
                     </a>
                     <nav className="flex gap-4 text-black font-semibold text-lg">
-                        <h1>Temperature Converter App</h1>
+                        <a href="/">Temperature Converter App</a>
                     </nav>
                 </div>
             </header>
