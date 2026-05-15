e8b8f1c363636e5194fae90b9d0b869035bced69
evitsapara <evi.tsapi@gmail.com>
2026-05-15 16:04:59 +0300

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Layout from "./components/Layout.tsx";
+
+function App() {
+
+  return (
+    <>
+      <Layout>
+      </Layout>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+    return (
+        <>
+            <header className="bg-cf-gray fixed w-full">
+                <div className="container mx-auto px-4 flex items-center justify-between">
+                    <a href="/">
+                        <img src="https://www.pngkey.com/png/detail/297-2976588_temperature-conversion-convert-temperature-icon.png" alt="Temperature Conversion" className="my-4 h-16"/>
+                    </a>
+                    <nav className="flex gap-4 text-black font-semibold text-lg">
+                        <h1>Temperature Converter App</h1>
+                    </nav>
+                </div>
+            </header>
+        </>
+    )
+}
+export default Header;
